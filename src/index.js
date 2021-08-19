ffe47722804a1d3fa8de643f1183194076ea7f6e
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2021-08-19 18:48:08 +0900

    componentの違い

--- src/index.js
+++ src/index.js
@@ -4,51 +4,33 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ES6~ 代入不可
-const name = {
-  firstName: '宜久',
-  lastName: '飯島',
-};
-
-const someHtml = { __html: '<h2>これはH2タグです</h2>' };
-
-// ES6~ 代入可能
-let count = 0;
-
-for (let i = 0; i < 10; i++) {
-  count++;
-}
-
+// Functional Component or StatelessComponent
+// 基本こっちの下記かた
 function Hello() {
   return (
     <section>
-      <h1>
-        こんにちわ{name.lastName + name.firstName}
-        {count + 1}回目の訪問ですね
-      </h1>
+      <h1>こんにちわ。</h1>
       <p id='hello'>Helloコンポーネントが表示されました</p>
-      <br />
-      {/* エスケープしない場合はdangerouslySetInnerHTML */}
-      <span dangerouslySetInnerHTML={someHtml} />
-
-      <div
-        className='imageBox'
-        htmlFor='foorrr'
-        style={{ marginTop: '10px', padding: '20px' }}
-        data-role='sss'
-      >
-        <img
-          src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-          alt='Google'
-        />
-      </div>
     </section>
   );
+}
+
+// ClassComponent
+class Mourning extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>おはようございます</h1>
+        <p id='morning'>Mourningコンポーネントが表示されました</p>
+      </section>
+    );
+  }
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <Hello />
+    <Mourning />
   </React.StrictMode>,
   document.getElementById('root')
 );
