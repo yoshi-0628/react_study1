4764fefae98c21b52f91469eb5a3c43061ed386c
yoshi-0628 <59409129+yoshi-0628@users.noreply.github.com>
2021-08-19 18:15:49 +0900

    講座途中

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// ES6~ 代入不可
+const name = {
+  firstName: '宜久',
+  lastName: '飯島',
+};
+
+const someHtml = { __html: '<h2>これはH2タグです</h2>' };
+
+// ES6~ 代入可能
+let count = 0;
+
+for (let i = 0; i < 10; i++) {
+  count++;
+}
+
+function Hello() {
+  return (
+    <section>
+      <h1>
+        こんにちわ{name.lastName + name.firstName}
+        {count + 1}回目の訪問ですね
+      </h1>
+      <p id='hello'>Helloコンポーネントが表示されました</p>
+      <br />
+      {/* エスケープしない場合はdangerouslySetInnerHTML */}
+      <span dangerouslySetInnerHTML={someHtml} />
+
+      <div
+        className='imageBox'
+        htmlFor='foorrr'
+        style={{ marginTop: '10px', padding: '20px' }}
+        data-role='sss'
+      >
+        <img
+          src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+          alt='Google'
+        />
+      </div>
+    </section>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
